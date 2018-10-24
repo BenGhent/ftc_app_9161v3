00bcadff28aee829b6f6676dc9942c6699f583a5
Satellite2015\Jordan <jordanic101@gmail.com>
2018-10-23 21:48:05 -0500

    created teleop and hardwarre files

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop_1.java
@@ -0,0 +1,49 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.util.Range;
+
+
+
+@TeleOp(name="Teleop_1", group="Iterative Opmode")
+@Disabled
+public class Teleop_1 extends OpMode
+{
+    DcMotor frontLeftWheel;
+    DcMotor backLeftWheel;
+    DcMotor frontRightWheel;
+    DcMotor backRightWheel;
+
+    @Override
+    public void init() {
+
+    }
+
+
+    @Override
+    public void init_loop() {
+    }
+
+
+    @Override
+    public void start() {
+
+
+    }
+
+
+    @Override
+    public void loop() {
+
+    }
+
+
+    @Override
+    public void stop() {
+    }
+
+}
